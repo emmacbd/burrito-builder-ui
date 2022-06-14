0a9cdce4127986b492b54e18e43fc46f72de5da9
Emma Brooke-Davidson <ecbrooked@gmail.com>
2022-06-14 10:13:32 -0500

    Fix initial render bug

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
-import {getOrders} from '../../apiCalls';
+import { getOrders } from '../../apiCalls';
 import Orders from '../../components/Orders/Orders';
 import OrderForm from '../../components/OrderForm/OrderForm';
 
 class App extends Component {
   constructor(props) {
     super();
+    this.state = {
+      orders: []
+    }
   }
 
   componentDidMount() {
     getOrders()
+      .then(data => this.setState({ data }))
       .catch(err => console.error('Error fetching:', err));
   }
 
@@ -22,7 +26,7 @@ class App extends Component {
           <OrderForm />
         </header>
 
-        <Orders orders={this.state.orders}/>
+        <Orders orders={this.state.orders} />
       </main>
     );
   }
